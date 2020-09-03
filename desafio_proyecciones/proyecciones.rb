cade00f83ae5871227192741dc0df42a4efb3155
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-02 20:07:11 -0400

    adding folder and instructions

---
+++ desafio_proyecciones/proyecciones.rb
@@ -0,0 +1,14 @@
+# Crear el programa proyecciones.rb además tendremos un archivo con las proyecciones de venta
+# de cada mes del próximo año. Este archivo debe llamarse ventas_base.db y estar en el misma
+# carpeta de trabajo que el código. Para realizar el ejercicio puedes agregar como contenido el
+# siguiente.
+
+# El desafío consiste en hacer 2 simulaciones.
+# Las ventas totales dado que en la primera mitad del semestre se vende un 10% más.
+# Las ventas totales dado que en la segunda mitad del semestre se vende un 20% más.
+# El resultado debe ser presentado un archivo llamado resultados.data . Los datos deben contener
+# máximo 2 decimales y estar cada uno en una sola línea.
+
+# Uso:
+# ruby proyecciones.rb
+# Contenido resultados.data
