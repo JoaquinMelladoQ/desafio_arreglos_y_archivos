9daa2b4b8ff88924fc77583a47e0c7d052cc0286
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-02 22:52:11 -0400

    erasing all comments

--- d_proyecciones/proyecciones.rb
+++ d_proyecciones/proyecciones.rb
@@ -1,34 +1,4 @@
-# Crear el programa proyecciones.rb además tendremos un archivo con las proyecciones de venta
-# de cada mes del próximo año. Este archivo debe llamarse ventas_base.db y estar en el misma
-# carpeta de trabajo que el código. Para realizar el ejercicio puedes agregar como contenido el
-# siguiente.
 
-# El desafío consiste en hacer 2 simulaciones.
-# Las ventas totales dado que en la primera mitad del semestre se vende un 10% más.
-# Las ventas totales dado que en la segunda mitad del semestre se vende un 20% más.
-# El resultado debe ser presentado un archivo llamado resultados.data . Los datos deben contener
-# máximo 2 decimales y estar cada uno en una sola línea.
-
-# Uso:
-# ruby proyecciones.rb
-# Contenido resultados.data
-
-# 231231.32
-# 879879.43
-
-# Tips:
-# El output en pantalla no es necesario pero no afecta la evaluación, se debe crear el archivo
-# resultados.data y debe cumplir con el formato establecido.
-# Tener cuidado con los datos de entrada, son strings y deben ser tratados como float.
-# Se recomienda crear un método que reciba cuatro argumentos:
-# El arreglo con los datos.
-# El aumento
-# Desde qué índice se debe leer el arreglo
-# Hasta qué índice se debe leer el arreglo
-# Investigar respecto a ruby format number
-# El archivo con el que se probará el ejercicio tendrá distintos datos.
-
-#creating method total sales
 def total_sales( param )
     sum = 0
     param.each do | e |
@@ -37,18 +7,14 @@ def total_sales( param )
     return sum
 end
 
-#reading data from file
 data__base = open("ventas_base.db").read
 
-#array modified
 param = data__base.split(',')
 
-#transforming values to floating numbers and pushing changes to original array
 param.map! do | e |
     e = e.to_f
 end
 
-#pushing to an empy array the results
 first_semester = []
 second_semester = []
 
@@ -68,11 +34,9 @@ for i in 0..11 do
     end
 end
 
-#using arrays to sending to method and new file
 first_semester_total = total_sales(first_semester)
 second_semester_total = total_sales(second_semester)
 
-#creating new file
 open("resultados.data", "w") do | file |
     file << "#{first_semester_total}\n"
     file << "#{second_semester_total}"
