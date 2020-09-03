41dcbb017439fededfd8106270fe22476f3afb37
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-03 12:55:22 -0400

    final test

--- d_calculo_notas2/calculo_notas2.rb
+++ d_calculo_notas2/calculo_notas2.rb
@@ -1,19 +1,3 @@
-# Se pide:
-# Crear un archivo llamado calculo_notas2.rb .
-# Crear un método llamado notas_mas_alta dentro del archivo calculo_notas2.rb que
-# reciba un arreglo con el nombre y notas de un alumno y devuelva una arreglo con la nota mas
-# alta de cada alumno.
-
-# Tips:
-# Devolver no es mostar en pantalla.
-# El archivo no será el mismo mostrado, pueden ser más columnas y más filas.
-# El espacio principal del programa no será probado, puedes ocuparlo para probar el código
-# llamando a los métodos, recuerda transformar las notas a número.
-# El nombre siempre será el primer elemento.
-# Todos los alumnos tendrán la misma cantidad de notas.
-# Cuidado con los tipos de datos, el arreglo contiene strings y números.
-
-#creating method
 def nota_mas_alta( param )
     notas_mas_altas = []
     param.each do | e |
@@ -23,11 +7,9 @@ def nota_mas_alta( param )
     return notas_mas_altas
 end
 
-#requiring to read data
 require "csv"
 data__base = CSV.open("notas.data", converters: :all).readlines 
 
-#testing
 print "#{nota_mas_alta(data__base)}\n"
 
 
