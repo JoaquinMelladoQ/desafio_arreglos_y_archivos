9464694220f925c0ee0b115653fb35795883b63b
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-02 20:08:45 -0400

    adding tips to proyecciones

--- desafio_proyecciones/proyecciones.rb
+++ desafio_proyecciones/proyecciones.rb
@@ -12,3 +12,18 @@
 # Uso:
 # ruby proyecciones.rb
 # Contenido resultados.data
+
+# 231231.32
+# 879879.43
+
+# Tips:
+# El output en pantalla no es necesario pero no afecta la evaluación, se debe crear el archivo
+# resultados.data y debe cumplir con el formato establecido.
+# Tener cuidado con los datos de entrada, son strings y deben ser tratados como float.
+# Se recomienda crear un método que reciba cuatro argumentos:
+# El arreglo con los datos.
+# El aumento
+# Desde qué índice se debe leer el arreglo
+# Hasta qué índice se debe leer el arreglo
+# Investigar respecto a ruby format number
+# El archivo con el que se probará el ejercicio tendrá distintos datos.
