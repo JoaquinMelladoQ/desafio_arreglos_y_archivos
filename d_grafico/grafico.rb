ae64425ea4b6c251ecb34c363a32b56e543c1201
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-03 13:06:02 -0400

    working well method was created and tested

--- d_grafico/grafico.rb
+++ d_grafico/grafico.rb
@@ -19,3 +19,18 @@
 # |********************
 # >--------------------
 # 1 2 3 4 5 6 7 8 9 10
+
+
+def chart( param )
+    maximum = param.max()
+    param.each do | e |
+        puts "|" + "*" * ( e * 2 )
+    end
+    puts ">" + "--" * ( maximum )
+    1.upto( maximum ) do | i |
+        print " #{ i }"
+    end
+    return
+end
+
+chart([5, 3, 2, 5, 10])
